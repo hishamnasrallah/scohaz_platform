12bd0c071fcaa44315876ed6076dd6e7e8d96425
hisham.nasrallah <hisham.nasralla@gmail.com>
2025-07-22 10:14:41 +0300

    inquiry urls

--- mockapi/urls.py
+++ mockapi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from mockapi.views.beneficiary_info import get_user_info
-
+from mockapi.views.payment import process_payment, check_payment_status
 
 urlpatterns = [
     path('user_info/<str:national_number>/', get_user_info),
+    path('payment/process/', process_payment, name='process_payment'),
+    path('payment/status/<str:transaction_id>/', check_payment_status, name='check_payment_status'),
 ]
